cf185e7cb91372663dcbf34ceca29ba35c0d40cc
milesman34 <milesmanmusic@yahoo.com>
2022-05-16 17:56:33 -0400

    Bottom tab buttons switch between pages

--- lumps.py
+++ lumps.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QWidget, QTabWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QWidget, QStackedWidget
 
 from widgets.BottomTabButton import BottomTabButton
 
@@ -36,18 +36,29 @@ class Lumps(QMainWindow):
         upperTabBar = QLabel("Upper Part")
         upperTabBar.setStyleSheet("background-color: rgb(235, 235, 235); border-bottom: 2px solid black;")
 
+        # Creates the central part of the app
+        self.centralPart = QStackedWidget()
+
+        # Adds the two currently existing pages (pages not implemented yet)
+        self.centralPart.addWidget(QLabel("Dice"))
+        self.centralPart.addWidget(QLabel("Scoreboard"))
+
+        # Sets the current page
+        self.centralPart.setCurrentIndex(enums.AppPage.DICE.value)
+
         # Creates the bottom bar and gives it a border
         bottomTabBar = QWidget()
         
         # The object has its own name so that the children do not also get the same styling
         bottomTabBar.setObjectName("bottomTabBar")
         bottomTabBar.setStyleSheet("QWidget#bottomTabBar {background-color: rgb(235, 235, 235); border-top: 2px solid black;}")
+        bottomTabBar.setContentsMargins(0, 0, 0, 0)
         
         bottomTabBarLayout = QHBoxLayout(self)
 
         # Creates the two buttons for the bottom tab bar to switch pages
-        self.dicePageButton = BottomTabButton(self, "Dice")
-        self.scoreboardPageButton = BottomTabButton(self, "Scoreboard")
+        self.dicePageButton = BottomTabButton(self, "Dice", enums.AppPage.DICE)
+        self.scoreboardPageButton = BottomTabButton(self, "Scoreboard", enums.AppPage.SCOREBOARD)
 
         # Adds the two button widgets to the bottom tab bar
         bottomTabBarLayout.addWidget(self.dicePageButton)
@@ -57,7 +68,7 @@ class Lumps(QMainWindow):
 
         # Adds the widgets to the outer layout
         outerLayout.addWidget(upperTabBar, stretch=1)
-        outerLayout.addWidget(QLabel("Middle Part"), stretch=18)
+        outerLayout.addWidget(self.centralPart, stretch=18)
         outerLayout.addWidget(bottomTabBar, stretch=1)
 
         # Creates the main widget
--- widgets/BottomTabButton.py
+++ widgets/BottomTabButton.py
@@ -1,10 +1,16 @@
 from PyQt5 import QtWidgets
 
+from enums import AppPage
+
+
 class BottomTabButton(QtWidgets.QPushButton):
-    def __init__(self, parent=None, text=""):
+    def __init__(self, parent, text, app_page: AppPage):
         super(BottomTabButton, self).__init__(parent)
 
         # Sets the text of the button
         self.setText(text)
 
         self.setStyleSheet("background-color: rgb(235, 235, 235);")
+
+        # Adds the on-click event
+        self.clicked.connect(lambda e: parent.centralPart.setCurrentIndex(app_page.value))
